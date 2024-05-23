3c7c9e1ec2646f18717349f496695a2f804fb305
Kobe Dieryck <kobe.dieryck@icloud.com>
2024-05-23 19:07:45 +0200

    working on benternet

--- Benternet-zmq/main.py
+++ Benternet-zmq/main.py
@@ -1,29 +1,49 @@
 import zmq
 
-class ZMQMessenger:
-    def __init__(self, address):
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.REQ)
-        self.socket.connect(address)
-
-    def send_message(self, message):
-        self.socket.send_string(message)
-
-    def receive_message(self):
+class ZMQ_service:
+    def __init__(self, pushaddress, subaddress):
+        self.push_context = zmq.Context()
+        self.push_socket = self.push_context.socket(zmq.PUSH)
+        self.push_socket.connect(pushaddress)
+        
+        self.sub_context = zmq.Context()
+        self.sub_socket = self.sub_context.socket(zmq.SUB)
         try:
-            if self.socket.poll(timeout=10000) != 0:
-                response = self.socket.recv_string()
-                return response
-            else:
-                print("No message received")
-                return None
+            self.sub_socket.connect(subaddress)
         except zmq.error.ZMQError as e:
-            print("Error receiving message:", e)
+            print(f"Failed to connect sub socket to address {subaddress}: {e}")
+            exit(1)
+            
+    def subscribe(self, topic):
+        self.sub_socket.setsockopt_string(zmq.SUBSCRIBE, topic)
+        
+    def send_message(self, message):
+        self.push_socket.send_string(message)
+
+    def receive_message(self, timeout=10000):
+        poller = zmq.Poller()
+        poller.register(self.sub_socket, zmq.POLLIN)
+        socks = dict(poller.poll(timeout))
+        if self.sub_socket in socks:
+            response = self.sub_socket.recv_string()
+            return response
+        else:
+            print("Timeout.")
             return None
 
-# Example usage
-messenger = ZMQMessenger("tcp://localhost:24041")
-# messenger.send_message("Hello, World!")
+
+
+print("starting")
+messenger = ZMQ_service("tcp://benternet.pxl-ea-ict.be:24041", "tcp://benternet.pxl-ea-ict.be:24042")
+print("messenger created")
+print("message sent")
+messenger.subscribe("Ferre")  # Subscribe to all topics
+print("receiving messages")
 response = messenger.receive_message()
 if response is not None:
     print("Received:", response)
+
+while True:
+    message = messenger.receive_message()
+    if response is not None:
+        print("Received:", message)
